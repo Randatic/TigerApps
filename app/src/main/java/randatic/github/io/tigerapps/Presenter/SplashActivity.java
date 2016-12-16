8a63c2d1c8c9af1fac11c285f84bb43a18f7716b
Randatic <randatic@gmail.com>
2016-12-15 18:43:19 -0800

    Adding most things now

---
+++ app/src/main/java/randatic/github/io/tigerapps/Presenter/SplashActivity.java
@@ -0,0 +1,42 @@
+package randatic.github.io.tigerapps.Presenter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+import randatic.github.io.tigerapps.R;
+
+/**
+ * Created by nell on 12/15/2016.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    CountDownTimer timer;
+
+    @Override
+    protected void onCreate(Bundle savedStateInstance) {
+        super.onCreate(savedStateInstance);
+        setContentView(R.layout.activity_splash);
+
+        timer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                endSplashScreen();
+            }
+        };
+
+    }
+
+    private void endSplashScreen() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
